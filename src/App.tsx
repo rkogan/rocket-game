76b4d47e592713282f29fa0612fa86b4b137ae4a
Rob Kogan <rkogan@gmail.com>
2024-02-03 22:03:03 -0800

    Updates styling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,18 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+import { useState, useEffect, KeyboardEvent } from 'react'
+import classNames from 'classnames'
+import './App.css'
+
+const gravity = -0.5
+const thrust = 5
+const maxLandingSpeed = -8
+const initialState = {
+  altitude: 100,
+  speed: 5
+}
 
 function App() {
-  const gravity = -0.5
-  const thrust = 5
-  const maxLandingSpeed = -8
-  const initialState = {
-    altitude: 100,
-    speed: 5
-  }
-
   const [rocket, setRocket] = useState(initialState)
   const [crash, setCrash] = useState(false)
-
-  // Call update every 50ms (20 frames/sec).
-  const DELAY_MS = 50
-
-  const fireThruster = () => {
-    if (crash) return
-
-    setRocket(({ altitude, speed: currSpeed }) => ({
-      altitude,
-      speed: currSpeed + thrust
-    }))
-  }
-
-  const resetGame = () => {
-    setCrash(false)
-    setRocket(initialState)
-  }
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -51,24 +35,48 @@ function App() {
           speed: newSpeed
         }
       })
-    }, DELAY_MS)
+    }, 50)
 
     return () => clearInterval(interval)
   }, [gravity, maxLandingSpeed, rocket.speed])
 
+  const resetGame = () => {
+    setCrash(false)
+    setRocket(initialState)
+  }
+
+  const fireThruster = () => {
+    if (crash) return
+
+    setRocket(({ altitude, speed: currSpeed }) => ({
+      altitude,
+      speed: currSpeed + thrust
+    }))
+  }
+
+  const keyPressed = (e: KeyboardEvent<HTMLDivElement>) => e.key === ' ' && fireThruster()
+
   return (
-    <div className="app" onMouseDown={fireThruster} onTouchStart={fireThruster}>
-    <header className="header">
-        <div className="title">Rocket Game</div>
-        <div className="description">Land the rocket using the thruster (clicks, touches, or spacebar).</div>
+    <div className='app' onMouseDown={fireThruster} onTouchStart={fireThruster} onKeyDown={keyPressed} tabIndex={0}>
+      <header className='header'>
+        <div className='title'>Rocket Game</div>
+        <div className='description'>
+          Land the rocket using the thruster (clicks, touches, or spacebar).
+        </div>
       </header>
 
-      <div id="rocket" className={crash ? 'crash' : ''} style={{ bottom: `${rocket.altitude}px` }}>
-        <div className="fire"></div>
-        <div id="restart" onClick={resetGame}>RESTART</div>
+      <div
+        id='rocket'
+        className={classNames({ crash })}
+        style={{ bottom: `${rocket.altitude}px` }}
+      >
+        <div className='fire'></div>
+        <div id='restart' onClick={resetGame}>
+          RESTART
+        </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
